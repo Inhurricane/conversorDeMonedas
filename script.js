0d7e5d165e18069375be7d1dc51e70e799eedbb2
Santiago Pino <128741557+Inhurricane@users.noreply.github.com>
2024-03-28 00:42:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 var nombreUsuario = prompt("Ingrese su nombre");
+nombreUsuario = nombreUsuario.charAt(0).toUpperCase() + nombreUsuario.slice(1).toLowerCase(); // convierte la primera letra del nombre en mayuscula y el resto en minuscula
 
 var saludo = alert("Hola " + nombreUsuario + ", bienvenido a nuestro conversor de monedas...");
 
 var monedaUsuario;
-var monedasValidas = ["USD", "COP", "EUR", "YEN", "BTC"];
+var monedasValidas = ["USD", "COP", "EUR", "YEN", "BTC"]; // array con las monedas validas
+
+// arrays con las monedas validas para cada moneda
+var monedaValidaUSD = ["COP", "EUR", "YEN", "BTC"]; 
+var monedaValidaCOP = ["USD", "EUR", "YEN"]; 
+var monedaValidaEUR = ["COP", "USD", "YEN", "BTC"]; 
+var monedavalidaYEN = ["COP", "USD", "EUR"];
+var monedavalidaBTC = ["COP", "EUR", "YEN"];
 
 do {
     monedaUsuario = prompt("Por favor ingrese la moneda que desea convertir \nLas opciones disponibles son: USD, COP, EUR, YEN y BTC");
-} while (!monedasValidas.includes(monedaUsuario.toUpperCase()));
+} while (!monedasValidas.includes(monedaUsuario.toUpperCase())); // valida que la moneda ingresada sea valida entre las opciones, si no lo es, pide que se ingrese de nuevo
 
-monedaUsuario = monedaUsuario.toUpperCase();
+monedaUsuario = monedaUsuario.toUpperCase(); // convierte la moneda ingresada en mayusculas para que el Switch pueda leerla
+
+//En el ejercicio se supone una tasa de cambio estimada
 
 switch (monedaUsuario) {
 
     case "USD":
-        var USD = prompt("La moneda seleccionada es dolar, por favor ingrese la cantidad de dolares que desea convertir");
-        var conversionUSD = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN y BTC)");
-        
+        var USD = parseFloat(prompt("La moneda seleccionada es dolar, por favor ingrese la cantidad de dolares que desea convertir"));
+        do {
+            conversionUSD = prompt("Por favor ingrese la moneda que desea convertir \nLas opciones disponibles son: COP, EUR, YEN y BTC");
+        } while (!monedaValidaUSD.includes(conversionUSD.toUpperCase())); // valida que la moneda ingresada sea valida entre las opciones, si no lo es, pide que se ingrese de nuevo
+        conversionUSD = conversionUSD.toUpperCase();
+
+        //var conversionUSD = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN y BTC)");
+        //conversionUSD = conversionUSD.toUpperCase(); // anteriormente solo validaba la conversion a mayusculas, ahora valida que la conversion sea valida entre las opciones
+
         if (conversionUSD == "COP") {
             var cotizacionEnCOP = 3887.00;
             var conversionTotal = USD * cotizacionEnCOP;
@@ -44,9 +60,14 @@ switch (monedaUsuario) {
         break;
 
     case "COP":
-        var COP = prompt("La moneda seleccionada es peso colombiano, por favor ingrese la cantidad de pesos que desea convertir");
-        var conversionCOP = prompt("Por favor ingrese la moneda a la que desea convertir los pesos colombianos (COP, EUR, YEN)");
+        var COP = parseInt(prompt("La moneda seleccionada es peso colombiano, por favor ingrese la cantidad de pesos que desea convertir"));
+        do {
+            conversionCOP = prompt("Por favor ingrese la moneda que desea convertir \nLas opciones disponibles son: USD, EUR, YEN");
+        } while (!monedaValidaCOP.includes(conversionCOP.toUpperCase()));
 
+        // var conversionCOP = prompt("Por favor ingrese la moneda a la que desea convertir los pesos colombianos (USD, EUR, YEN)");
+        // conversionCOP = conversionCOP.toUpperCase();
+        
         if (conversionCOP == "USD") {
             var cotizacionEnUSD = 0.00026;
             var conversionTotal = COP * cotizacionEnUSD;
@@ -68,8 +89,13 @@ switch (monedaUsuario) {
         break;
 
     case "EUR":
-        var EUR = prompt("La moneda seleccionada es euro, por favor ingrese la cantidad de euros que desea convertir");
-        var conversionEUR = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN y BTC)");
+        var EUR = parseFloat(prompt("La moneda seleccionada es euro, por favor ingrese la cantidad de euros que desea convertir"));
+        do {
+            conversionEUR = prompt("Por favor ingrese la moneda que desea convertir \nLas opciones disponibles son: COP, USD, YEN y BTC");
+        } while (!monedaValidaEUR.includes(conversionEUR.toUpperCase()));
+
+        // var conversionEUR = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN y BTC)");
+        // conversionEUR = conversionEUR.toUpperCase();
 
         if (conversionEUR == "COP") {
             var cotizacionEnCOP = 4213.24;
@@ -98,8 +124,13 @@ switch (monedaUsuario) {
         break;
     
     case "YEN":
-        var YEN = prompt("La moneda seleccionada es yen, por favor ingrese la cantidad de yenes que desea convertir");
-        var conversionYEN = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN)");
+        var YEN = parseFloat(prompt("La moneda seleccionada es yen, por favor ingrese la cantidad de yenes que desea convertir"));
+        do {
+            conversionYEN = prompt("Por favor ingrese la moneda que desea convertir \nLas opciones disponibles son: COP, USD, EUR");
+        } while (!monedavalidaYEN.includes(conversionYEN.toUpperCase()));
+        
+        // var conversionYEN = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN)");
+        // conversionYEN = conversionYEN.toUpperCase();
 
         if (conversionYEN == "COP") {
             var cotizacionEnCOP = 25.68;
@@ -122,9 +153,14 @@ switch (monedaUsuario) {
         break;
 
     case "BTC":
-        var BTC = prompt("Ha seleccionado Bitcoin, por favor ingrese la cantidad de bitcoins que desea convertir");
-        var conversionBTC = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN y BTC)");
-        
+        var BTC = parseFloat(prompt("Ha seleccionado Bitcoin, por favor ingrese la cantidad de bitcoins que desea convertir"));
+        do {
+            conversionBTC = prompt("Por favor ingrese la moneda que desea convertir \nLas opciones disponibles son: COP, USD, EUR y YEN");
+        } while (!monedavalidaBTC.includes(conversionBTC.toUpperCase()));
+
+        // var conversionBTC = prompt("Por favor ingrese la moneda a la que desea convertir los dolares (COP, EUR, YEN y BTC)");
+        // conversionBTC = conversionBTC.toUpperCase();
+
         if (conversionBTC == "COP") {
             var cotizacionEnCOP = 274091027.60;
             var conversionTotal = BTC * cotizacionEnCOP;
